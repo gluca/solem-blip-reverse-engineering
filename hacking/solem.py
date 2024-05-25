934c544292653604340478e2af33d367ce7b96c8
Gianluca <gianluca@magalotti.net>
2024-05-25 19:46:56 +0200

    Update solem.py
    minor changes

--- hacking/solem.py
+++ hacking/solem.py
@@ -1,7 +1,7 @@
 import struct
 import binascii
 import time
-from bluepy import btle
+from bluepy3 import btle
 '''
 Identified commands
 --------------------------------
@@ -28,106 +28,149 @@ stop manual:    | 31051500ff0000
 transmit:       | 3b00
 '''
 
-class BLIPNotification(btle.DefaultDelegate):
-    def __init__(self, params):
-        btle.DefaultDelegate.__init__(self)
-        print(f"Created delegate for handle {params}")
-        # ... initialise here
+class SolemBLIP:
 
-    def handleNotification(self, cHandle, data):       
-        # ... perhaps check cHandle
-        # ... process 'data'
-        btle.DefaultDelegate.__init__(self)       
-        print(f"Notification from {cHandle}: {binascii.hexlify(data)} {data}")
+    class BLIPNotification(btle.DefaultDelegate):
+        def __init__(self, params, debug=False):
+            btle.DefaultDelegate.__init__(self)
+            self.__debug = debug
+            if self.__debug:
+                print(f"Created delegate for handle {params}")
+            # ... initialise here
 
-def handleNotifications(per, wait):
-    while wait:
-        if per.waitForNotifications(0.3):
-            # handleNotification() was called
-            continue
-        else:
-            print("Waiting...")
-            wait = wait - 1
+        def handleNotification(self, cHandle, data):       
+            # ... perhaps check cHandle
+            # ... process 'data'
+            btle.DefaultDelegate.__init__(self)       
+            if self.__debug:
+                print(f"Notification from {cHandle}: {binascii.hexlify(data)}")
 
 
-def irrigatorOn():
-    return struct.pack(">HBHH",0x3105,0xa0,0x0001,0x0000)
+    def __handleNotifications(self,num):
+        while num and self.__notificationsEnabled:
+            if self.__blConnection.waitForNotifications(0.5):
+                # handleNotification() was called
+                num = num - 1
+            else:
+                if self.__debug:
+                    print("Waiting...")
 
-def irrigatorOff():
-    return struct.pack(">HBHH",0x3105,0xc0,0x0000,0x0000)
+    def __init__(self,address):
+        self.__debug = False
+        self.connected = False     
+        self.address = address
+        self.__blConnection = btle.Peripheral()
+        self.name = None
+        self.preferredParams =  b'0x00'
+        self.__notifyier = None
+        self.__writer = None
+        self.__notificationsEnabled = False
+        
+    def __writeCommand(self,cmd):
+        if self.__debug:
+            print(f"Sending command:{binascii.hexlify(cmd)}")
+        self.__writer.write(cmd)
+        self.__handleNotifications(3)
+        if self.__debug:
+            print("Committing (command:0x3b00)")
+        self.__writer.write(struct.pack(">H",0x3b00))
+        self.__handleNotifications(3)
+    
+    def on(self):
+        self.__writeCommand(struct.pack(">HBHH",0x3105,0xa0,0x0001,0x0000))
 
-def stopWatering():
-    return struct.pack(">HBHH",0x3105,0x15,0x00ff,0x0000)
+    def off(self):
+        self.__writeCommand(struct.pack(">HBHH",0x3105,0xc0,0x0000,0x0000))
 
-def commit():
-    return struct.pack(">H",0x3b00)
+    def stopWatering(self):
+        self.__writeCommand(struct.pack(">HBHH",0x3105,0x15,0x00ff,0x0000))
 
-def irrigatorOffDays(days):
-    return struct.pack(">HBHH",0x3105,0xc0,days,0x0000)
+    def offDays(self,days):
+        self.__writeCommand(struct.pack(">HBHH",0x3105,0xc0,days,0x0000))
 
-def startWateringAll(minutes):
-    secs=minutes*60
-    return struct.pack(">HBHH",0x3105,0x11,0x0000,secs)
+    def startWateringAll(self,minutes):
+        secs=minutes*60
+        self.__writeCommand(struct.pack(">HBHH",0x3105,0x11,0x0000,secs))
 
-def startWatering(station,minutes):
-    secs=minutes*60
-    return struct.pack(">HBBBH",0x3105,0x12,station,0x00,secs)
+    def startWatering(self,station,minutes):
+        secs=minutes*60
+        self.__writeCommand(struct.pack(">HBBBH",0x3105,0x12,station,0x00,secs))
 
-def runProgram(program):
-    return struct.pack(">HBHH",0x3105,0x14,program,0x0000)
+    def runProgram(self,program):
+        self.__writeCommand(struct.pack(">HBHH",0x3105,0x14,program,0x0000))
+    
+    def enableNotifications(self):
+        self.__blConnection.setDelegate(self.BLIPNotification(self.__notifyier.getHandle(), self.__debug))
+        self.__blConnection.writeCharacteristic(self.__notifyier.getHandle()+1, b"\x01\x00")
+        self.__notificationsEnabled = True
+        
+    def disableNotifications(self):
+        self.__blConnection.setDelegate(
+                    self.BLIPNotification(self.__notifyier.getHandle(), self.__debug))
+        self.__blConnection.writeCharacteristic(self.__notifyier.getHandle()+1, b"\x00\x00")                
+        self.__notificationsEnabled = False
 
-def resilientConnect(address, retries):
-    global per
-    while retries:
-        try:
-            per=btle.Peripheral(address,btle.ADDR_TYPE_RANDOM)
-            return True
-        except btle.BTLEException as e:
-            print(f"BLE Exception:", e)
-            retries = retries - 1
-            if retries > 0:
-                time.sleep(3)
-    return False
 
-per = btle.Peripheral()
+    def connect(self, retries, sleep = 2):
+        self.connected = False
+        if self.__debug:
+            print("Connecting")
+        
+        while retries:
+            try:
+                self.__blConnection=btle.Peripheral(self.address,btle.ADDR_TYPE_RANDOM)
+                self.connected = True
+                break
+            except btle.BTLEException as e:
+                retries = retries - 1
+                if self.__debug:
+                    print("BLE Exception:", e)
+                    print(f"Remaining: {retries} tentatives")
+                if retries > 0:
+                    time.sleep(sleep)
 
+        if not self.connected:
+            raise btle.BTLEException("Unable to connect after reties")
+        else:    
+            if self.__debug:
+                print("Connected!");
+            characteristics = self.__blConnection.getCharacteristics()
+            
+            for characteristic in characteristics:
+                #print("{}, hnd={}, supports {}".format(characteristic, hex(characteristic.handle), characteristic.propertiesToString()))
+                if characteristic.handle == 0x02:
+                    self.name=characteristic.read().decode("utf-8")
+                elif characteristic.handle == 0x04:
+                    app = characteristic.read()
+                elif characteristic.handle == 0x06:
+                    self.preferredParams = characteristic.read()
+                elif characteristic.handle == 0x0d:
+                    self.__notifyier = characteristic
+                elif characteristic.handle == 0x10:
+                    self.__writer = characteristic
+
+    def disconnect(self):
+        if self.connected:
+            try:
+                self.__blConnection.disconnect();
+            except btle.BTLEException as e:
+                if self.__debug:
+                    print("BLE Exception while disconnecting:", e)
+        self.connected = False
 try:
-    if not resilientConnect("C8:B9:61:0A:47:FD",10):
-        print(f"Failed to connect")
-        exit
-    print(f"Connected to Solem")
-    characteristics = per.getCharacteristics()
-    print(f"charachteristics read")
-    for characteristic in characteristics:
-        print("{}, hnd={}, supports {}".format(characteristic, hex(characteristic.handle), characteristic.propertiesToString()))
-        #if characteristic.uuid == '00002a04-0000-1000-8000-00805f9b34fb':
-        #    bytes = characteristic.read()
-        #    print("Read {}", struct.unpack('BBBBBBH', bytes))
-
-        if characteristic.uuid == '108b0002-eab5-bc09-d0ea-0b8f467ce8ee':
-            characteristicWrite = characteristic
-
-        elif characteristic.uuid == '108b0003-eab5-bc09-d0ea-0b8f467ce8ee':
-            characteristicNotify = characteristic
-
-    per.setDelegate(BLIPNotification(characteristicNotify.getHandle()))
-
-    # Setup to turn notifications on, e.g.
-    per.writeCharacteristic(characteristicNotify.getHandle()+1, b"\x01\x00")
-
-    #3105-15-00ff-0000 - stop any manual watering program
-    print(f"writing command")
-    characteristicWrite.write(stopWatering())
-    handleNotifications(per, 1)
-    print(f"committing")
-    characteristicWrite.write(commit())
-    handleNotifications(per, 3)
-    
-    
+    irrigatore=SolemBLIP("C8:B9:61:0A:47:FD")
+    print("Connecting...")
+    irrigatore.connect(10)
+    irrigatore.enableNotifications();
+    print(f"Name: {irrigatore.name}")
+    print("Water brain on")
+    irrigatore.on()
+    print("Stop Watering")
+    irrigatore.stopWatering();
+    print("Stopped")
 except btle.BTLEException as e:
-    print(f"BLE Exception:", e)
+    print("BLE Exception:", e)
 
 finally:
-    per.disconnect() 
-    print(f"Done.")
-s
+    irrigatore.disconnect()
+    print("Done.")
